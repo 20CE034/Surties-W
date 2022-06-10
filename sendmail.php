6cc403932abf67854c3c9d2c3584aa768e78f778
Dev Gundalia <94553525+20CE034@users.noreply.github.com>
2022-06-10 14:57:03 +0530

    same files as server deploy

--- sendmail.php
+++ sendmail.php
@@ -1,14 +1,56 @@
 <?php
+$user = $_POST["name"];
+$email = $_POST["email"];
+$mess = $_POST["message"];
+$number = $_POST["Contact"];
+$subject = $_POST["subject"];
+$message = "<html>
+<body>
+<style>
+tr {
+    font-size: x-large;
+}
+</style>
+  
+  <table>
+		<tr>
+			<td><h4><strong>Name</strong></h4></td>
+			<td>&nbsp;$user</td>
+		</tr>
+		<tr>
+			<td><h4><strong>Email</strong></h4></td>
+			<td>&nbsp;$email</td>
+		</tr>
+		<tr>
+			<td><h4><strong>Phone Number</strong></h4></td>
+			<td>&nbsp;$number</td>
+		</tr>
+		<tr>
+			<td><h4><strong>Subject</strong></h4></td>
+			<td>&nbsp;$subject</td>
+		</tr>
+		<tr>
+			<td><h4><strong>Message</strong></h4></td>
+			<td>&nbsp;$mess</td>
+		</tr>
+		<tr>
+			<td><h4><strong>Design And Developed by</strong></h4></td>
+			<td><strong>&nbsp;ATNO SOLUTIONS</strong></td>
+		</tr>
+</table>
+  
+</body>
+</html>";
+$headers[] = 'MIME-Version: 1.0';
+$headers[] = 'Content-type: text/html; charset=iso-8859-1';
 
-$email_address = $_POST["email"];
 if ($_POST["message"]) {
     mail(
-        "exlphy@gmail.com",
-        "Here is the sample subject line",
-        $_POST["message"] . "From: $email_address",
-        $additional_headers = [],
-        string $additional_params = ""
+        "data@surties.com",
+        "New Request From $user",
+        $message,
+        implode("\r\n", $headers)
     );
-    
 }
+header("Location: https://surties.com/");
 ?>
